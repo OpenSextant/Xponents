a91a34f530151e43107f356071bafbda26845736
Marc Ubaldino <ubaldino@mitre.org>
2014-05-06 11:52:08 -0400

    Migrate floating point formatting/benchmarking out to test source

--- Extraction/src/main/java/org/opensextant/extractors/xcoord/PrecisionScales.java
+++ Extraction/src/main/java/org/opensextant/extractors/xcoord/PrecisionScales.java
@@ -30,9 +30,9 @@ package org.opensextant.extractors.xcoord;
  #
  # http://en.wikipedia.org/wiki/Wikipedia:WikiProject_Geographical_coordinates#Precision
  */
-import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.text.DecimalFormat;
+
+import org.opensextant.util.TextUtils;
 
 /**
  * @author ubaldino
@@ -53,57 +53,76 @@ public class PrecisionScales {
      * decimal places.
      */
     public final static double[] DD_precision_list = {
-        LAT_DEGREE_PRECISION, // 0
-        LAT_DEGREE_PRECISION / 10, // 1
-        LAT_DEGREE_PRECISION / 100, // 2, ... etc.
-        LAT_DEGREE_PRECISION / 1000,
-        LAT_DEGREE_PRECISION / 10000,
-        LAT_DEGREE_PRECISION / 100000,
-        LAT_DEGREE_PRECISION / 1000000,
-        LAT_DEGREE_PRECISION / 10000000,
-        LAT_DEGREE_PRECISION / 100000000,
-        LAT_DEGREE_PRECISION / 1000000000,
-        LAT_DEGREE_PRECISION / 10000000000L,
-        LAT_DEGREE_PRECISION / 100000000000L,
-        LAT_DEGREE_PRECISION / 1000000000000L
-    };
+            // 
+            LAT_DEGREE_PRECISION, // 0
+            // 
+            LAT_DEGREE_PRECISION / 10, // 1
+            // 
+            LAT_DEGREE_PRECISION / 100, // 2, ... etc.
+            // 
+            LAT_DEGREE_PRECISION / 1000,
+            // 
+            LAT_DEGREE_PRECISION / 10000,
+            // Approx 1m precision at equator:
+            LAT_DEGREE_PRECISION / 100000,
+            // 
+            LAT_DEGREE_PRECISION / 1000000,
+            // 
+            LAT_DEGREE_PRECISION / 10000000,
+            // 
+            LAT_DEGREE_PRECISION / 100000000,
+            // 
+            LAT_DEGREE_PRECISION / 1000000000,
+            // 
+            LAT_DEGREE_PRECISION / 10000000000L,
+            // 
+            LAT_DEGREE_PRECISION / 100000000000L,
+            // 
+            LAT_DEGREE_PRECISION / 1000000000000L };
+
+    // Last entry above.
+    final static double FINEST_DD_PRECISION = LAT_DEGREE_PRECISION / 1000000000000L;
 
     /**
      * @param m
      */
     public static void setDDPrecision(GeocoordMatch m) {
-        m.precision.setDigits( count_DD_digits(m.lat_text) );
-        m.precision.setDigits( count_DD_digits(m.lon_text) );
+        m.precision.setDigits(count_DD_digits(m.lat_text));
+        m.precision.setDigits(count_DD_digits(m.lon_text));
 
         if (m.precision.digits < DD_precision_list.length) {
             m.precision.precision = DD_precision_list[m.precision.digits];
+        } else if (m.precision.digits > DD_precision_list.length) {
+            m.precision.precision = FINEST_DD_PRECISION;
         } else {
             m.precision.precision = DEFAULT_UNKNOWN_RESOLUTION;
         }
     }
-    
+
     /**
      * Return XCoord precision (+/- meters) in latitude.
      * @param lat
      * @return
      */
-    public static GeocoordPrecision getDDPrecision(String lat){
+    public static GeocoordPrecision getDDPrecision(String lat) {
         GeocoordPrecision prec = new GeocoordPrecision();
 
         // Find the number of digits used in lat.
-        prec.setDigits( count_DD_digits(lat) );
+        prec.setDigits(count_DD_digits(lat));
 
         // Determine the error given the number of digits
         if (prec.digits < DD_precision_list.length) {
             prec.precision = DD_precision_list[prec.digits];
+        } else if (prec.digits > DD_precision_list.length) {
+            prec.precision = FINEST_DD_PRECISION;
         } else {
             prec.precision = DEFAULT_UNKNOWN_RESOLUTION;
-        }        
+        }
         return prec;
     }
 
     /**
-     *
+     * Count the number of decimal places in a lat or lon text string.
      * @param lat
      * @return
      */
@@ -111,13 +130,13 @@ public class PrecisionScales {
         if (lat == null) {
             return 0;
         }
-
-        // Try calculating the sig-figs on the string repr of the number.
-        String[] parts = lat.split("[.]", 2);
-        if (parts.length == 2) {
-            return parts[1].length();
+        int x = lat.indexOf('.');
+        // "decimal point" Cannot be offset zero
+        if (x <= 0) {
+            return 0;
         }
-        return 0;
+        String decPart = lat.substring(x);
+        return TextUtils.count_digits(decPart);
     }
 
     /**
@@ -127,26 +146,17 @@ public class PrecisionScales {
      * @return
      */
     public static int count_DMS_digits(String lat) {
-        if (lat == null) {
-            return 0;
-        }
-
-        int digits = 0;
-        for (char c : lat.toCharArray()) {
-            if (Character.isDigit(c)) {
-                ++digits;
-            }
-        }
-        return digits;
+        return TextUtils.count_digits(lat);
     }
+
     /**
      *
      */
-    public static float DMS_MIN_PREC = 900;  // +/- 1.85 KM at equator
+    public static float DMS_MIN_PREC = 900; // +/- 1.85 KM at equator
     /**
      *
      */
-    public static float DMS_SEC_PREC = 15;   // +/- 0.03 KM at equator, 60th of a minute
+    public static float DMS_SEC_PREC = 15; // +/- 0.03 KM at equator, 60th of a minute
 
     /**
      * set precision on a DMS text coordinate
@@ -177,13 +187,12 @@ public class PrecisionScales {
             return;
         }
 
-
         if (m.hasSeconds()) {
             m.precision.precision = DMS_SEC_PREC;
-            m.precision.digits = 5;  // 1/3600th of a degree
+            m.precision.digits = 5; // 1/3600th of a degree
         } else if (m.hasMinutes()) {
             m.precision.precision = DMS_MIN_PREC;
-            m.precision.digits = 2;  // 1/60th of a degree
+            m.precision.digits = 2; // 1/60th of a degree
         }
     }
 
@@ -215,54 +224,47 @@ public class PrecisionScales {
         m.precision.precision = UTM_precision(m.getText());
         m.precision.digits = 5; // seconds resolution
     }
+
     /**
      * Precision appears in pairs, as we tolerate some typo/errors in MGRs.
      *
      */
-    public static float[] MGRS_precision_list = {
-        100000, // 1  GZD
-        100000, // 2  GZD
-        100000, // 3  GZD
-        100000, // 4  Q
-        100000, // 5  Q
-        100000, // 6, same as 5; odd # of chars usually means typo.  This would be first char in E/N'ing
-        // ---------------------------
-        10000, //  DEC PREC:  1 digit
-        10000, //
-        1000, //             3 digits
-        1000, // MINUTES PREC 3 digits
-        100, //              4 digits
-        100, //              4
-        10, // SECONDS PREC 6 digits
-        10,
-        1,
-        1,
-        0.1f, // 16
-        0.1f // 17
+    public static float[] MGRS_precision_list = { 100000, // 1  GZD
+            100000, // 2  GZD
+            100000, // 3  GZD
+            100000, // 4  Q
+            100000, // 5  Q
+            100000, // 6, same as 5; odd # of chars usually means typo.  This would be first char in E/N'ing
+            // ---------------------------
+            10000, //  DEC PREC:  1 digit
+            10000, //
+            1000, //             3 digits
+            1000, // MINUTES PREC 3 digits
+            100, //              4 digits
+            100, //              4
+            10, // SECONDS PREC 6 digits
+            10, 1, 1, 0.1f, // 16
+            0.1f // 17
     };
     /**
      *
      */
-    public static int[] MGRS2DEC_digits = {
-        0, // 1  GZD
-        0, // 2  GZD
-        0, // 3  GZD
-        0, // 4  Q
-        0, // 5  Q
-        0, // 6, same as 5; odd # of chars usually means typo.  This would be first char in E/N'ing
-        // ---------------------------
-        1, //  DEC PREC:  1 digit
-        1, //
-        3, //             3 digits
-        3, // MINUTES PREC 3 digits
-        4, //              4 digits
-        4, //              4
-        6, // SECONDS PREC 6 digits
-        6,
-        7,
-        7,
-        8, // 16
-        8 // 17
+    public static int[] MGRS2DEC_digits = { 0, // 1  GZD
+            0, // 2  GZD
+            0, // 3  GZD
+            0, // 4  Q
+            0, // 5  Q
+            0, // 6, same as 5; odd # of chars usually means typo.  This would be first char in E/N'ing
+               // ---------------------------
+            1, //  DEC PREC:  1 digit
+            1, //
+            3, //             3 digits
+            3, // MINUTES PREC 3 digits
+            4, //              4 digits
+            4, //              4
+            6, // SECONDS PREC 6 digits
+            6, 7, 7, 8, // 16
+            8 // 17
     };
 
     /**
@@ -273,6 +275,7 @@ public class PrecisionScales {
     public static float UTM_precision(String utm) {
         return 100f;
     }
+
     /**
      * Numeric formatters are cached here for performance. number of digits of
      * precision depends on lat (%2.12f) vs. lon (%3.12f)
@@ -280,18 +283,17 @@ public class PrecisionScales {
      */
     private static final DecimalFormat[] formatters;
 
-
     /* Static initialization here creates and caches all the numeric formatters
      * TODO: however at runtime the formatters are cloned (as of v1.6a) so is there any benefit?
      */
     static {
-      formatters = new DecimalFormat[12];
-      formatters[0] = new DecimalFormat("0");
-      StringBuilder buf = new StringBuilder("0.");
-      for (int i = 1; i < formatters.length; i++) {
-        buf.append('0');
-        formatters[i] = new DecimalFormat(buf.toString());
-      }
+        formatters = new DecimalFormat[12];
+        formatters[0] = new DecimalFormat("0");
+        StringBuilder buf = new StringBuilder("0.");
+        for (int i = 1; i < formatters.length; i++) {
+            buf.append('0');
+            formatters[i] = new DecimalFormat(buf.toString());
+        }
     }
 
     /**
@@ -302,7 +304,6 @@ public class PrecisionScales {
      * @return
      */
     public static String format(double f, int digits) {
-        //return (long)(f * digits)/digits);
         if (digits >= formatters.length) {
             return "" + f;
         }
@@ -314,123 +315,4 @@ public class PrecisionScales {
         return df.format(f);
     }
 
-    /**
-     * Rounds the number to the given digits past the decimal point. The
-     * code is pretty simple and is very fast as it doesn't use DecimalFormat
-     * or printf.  See <a href="http://stackoverflow.com/a/5806991/92186">here</a>
-     * for the algorithm.
-     *
-     * @param f
-     * @param digits
-     * @see tests in main()
-     * @deprecated TODO needs to be re-evaluated
-     * @author dsmiley
-     * @return String formatted number
-     */
-    public static String format2(double f, int digits) {
-        long rounderShift = 1;
-        //TODO DWS: the comment here is probably obsolete since adding the round()
-        // Must shift one past requested, as long conversion consistently rounds down
-        //   42.3 == 1 digit of precision
-        //   42.2999999 (in Java as a double, geesh.)
-        //   42.29999 *10 ==> LONG = 422
-        // Answer:  42.2  *Wrong.
-        //   42.29999 *100 ==> LONG = 4229
-        // Answer:  42.29  still wrong.
-        //
-        //TODO Math.pow() instead?  Same thing but we have a nice error check here.
-        for (int i = 0; i <= digits; i++) {
-            rounderShift *= 10;
-            if (rounderShift < 1) {
-                throw new IllegalArgumentException("Digits is too large: " + digits);
-            }
-        }
-        long fShifted = Math.round(f * rounderShift);//truncates trailing decimals by conversion to long
-        return "" + ((double) fShifted / rounderShift);
-    }
-
-    /**
-     *
-     * @param f
-     * @param digits
-     * @return
-     */
-    public static double bigDecimalRounder(double f, int digits) {
-        return new BigDecimal(f).setScale(digits, RoundingMode.HALF_EVEN).doubleValue();
-    }
-
-    /**
-     * @deprecated see setDDPrecision
-     * @param lat String version of decimal latitude, AS found raw
-     * @return
-     */
-    public static double DD_precision(String lat) {
-        if (lat == null) {
-            return -1;
-        }
-
-        if (!lat.contains(".")) {
-            return LAT_DEGREE_PRECISION;
-        }
-
-        // Try calculating the sig-figs on the string repr of the number.
-        String[] parts = lat.split("[.]", 2);
-        if (parts.length == 2) {
-            int sig_figs = parts[1].length();
-            if (sig_figs < DD_precision_list.length) {
-                return DD_precision_list[sig_figs];
-            }
-        }
-        return DEFAULT_UNKNOWN_RESOLUTION;
-    }
-    /**
-     * @deprecated Counting DMS digits is not accurate portrayal of precision
-     * 02:02:33N 6 digits 2:2:33N 4 digits
-     *
-     * Ah same precision, but different number of digits.
-     */
-    public static float[] DM_precision_list = {
-        DEFAULT_UNKNOWN_RESOLUTION, // 0
-        DEFAULT_UNKNOWN_RESOLUTION, // 1
-        LAT_DEGREE_PRECISION, // 2
-        10000f, // 3
-        1300f, // 4
-        220f, // 5
-        22f,
-        2.2f,
-        0.2f,
-        0.02f,
-        0.002f,
-        0.0002f};
-
-    /**
-     * @deprecated use setDMSPrecision()
-     * @param lat String version of decimal latitude, AS found raw
-     * @return
-     */
-    public static float DM_precision(String lat) {
-        if (lat == null) {
-            return -1;
-        }
-
-        /* Given an LAT / Y coordinate, determine the amount of precision therein.
-         * @TODO: redo,  given "x xx xx" vs. "xx xx xx"  -- resolution is the same. Should only count
-         * digits after
-         *
-         * LON / X coordinate can be up to 179.9999... so would always have one more leading digit than lat.
-         *
-         */
-        int digits = 0;
-        for (char ch : lat.toCharArray()) {
-            if (Character.isDigit(ch)) {
-                digits += 1;
-            }
-        }
-
-        if (digits > 11) {
-            return DEFAULT_UNKNOWN_RESOLUTION;
-        }
-
-        return DM_precision_list[digits];
-    }
 }
--- Extraction/src/test/java/PrecisionScalesTest.java
+++ Extraction/src/test/java/PrecisionScalesTest.java
@@ -15,6 +15,8 @@
 import org.junit.Test;
 import org.opensextant.extractors.xcoord.PrecisionScales;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.text.DecimalFormat;
 
 /**
@@ -22,6 +24,127 @@ import java.text.DecimalFormat;
  * @author jgibson
  */
 public class PrecisionScalesTest {
+    
+
+    /**
+     * Rounds the number to the given digits past the decimal point. The
+     * code is pretty simple and is very fast as it doesn't use DecimalFormat
+     * or printf.  See <a href="http://stackoverflow.com/a/5806991/92186">here</a>
+     * for the algorithm.
+     *
+     * @param f
+     * @param digits
+     * @see tests in main()
+     * @deprecated TODO needs to be re-evaluated
+     * @author dsmiley
+     * @return String formatted number
+     */
+    public static String format2(double f, int digits) {
+        long rounderShift = 1;
+        //TODO DWS: the comment here is probably obsolete since adding the round()
+        // Must shift one past requested, as long conversion consistently rounds down
+        //   42.3 == 1 digit of precision
+        //   42.2999999 (in Java as a double, geesh.)
+        //   42.29999 *10 ==> LONG = 422
+        // Answer:  42.2  *Wrong.
+        //   42.29999 *100 ==> LONG = 4229
+        // Answer:  42.29  still wrong.
+        //
+        //TODO Math.pow() instead?  Same thing but we have a nice error check here.
+        for (int i = 0; i <= digits; i++) {
+            rounderShift *= 10;
+            if (rounderShift < 1) {
+                throw new IllegalArgumentException("Digits is too large: " + digits);
+            }
+        }
+        long fShifted = Math.round(f * rounderShift);//truncates trailing decimals by conversion to long
+        return "" + ((double) fShifted / rounderShift);
+    }
+
+    /**
+     *
+     * @param f
+     * @param digits
+     * @return
+     */
+    public static double bigDecimalRounder(double f, int digits) {
+        return new BigDecimal(f).setScale(digits, RoundingMode.HALF_EVEN).doubleValue();
+    }
+
+    /**
+     * @deprecated see setDDPrecision
+     * @param lat String version of decimal latitude, AS found raw
+     * @return
+     */
+    public static double DD_precision(String lat) {
+        if (lat == null) {
+            return -1;
+        }
+
+        if (!lat.contains(".")) {
+            return PrecisionScales.LAT_DEGREE_PRECISION;
+        }
+
+        // Try calculating the sig-figs on the string repr of the number.
+        String[] parts = lat.split("[.]", 2);
+        if (parts.length == 2) {
+            int sig_figs = parts[1].length();
+            if (sig_figs < PrecisionScales.DD_precision_list.length) {
+                return PrecisionScales.DD_precision_list[sig_figs];
+            }
+        }
+        return PrecisionScales.DEFAULT_UNKNOWN_RESOLUTION;
+    }
+    /**
+     * @deprecated Counting DMS digits is not accurate portrayal of precision
+     * 02:02:33N 6 digits 2:2:33N 4 digits
+     *
+     * Ah same precision, but different number of digits.
+     */
+    public static float[] DM_precision_list = {
+        PrecisionScales.DEFAULT_UNKNOWN_RESOLUTION, // 0
+        PrecisionScales.DEFAULT_UNKNOWN_RESOLUTION, // 1
+        PrecisionScales.LAT_DEGREE_PRECISION, // 2
+        10000f, // 3
+        1300f, // 4
+        220f, // 5
+        22f,
+        2.2f,
+        0.2f,
+        0.02f,
+        0.002f,
+        0.0002f};
+
+    /**
+     * @deprecated use setDMSPrecision()
+     * @param lat String version of decimal latitude, AS found raw
+     * @return
+     */
+    public static float DM_precision(String lat) {
+        if (lat == null) {
+            return -1;
+        }
+
+        /* Given an LAT / Y coordinate, determine the amount of precision therein.
+         * @TODO: redo,  given "x xx xx" vs. "xx xx xx"  -- resolution is the same. Should only count
+         * digits after
+         *
+         * LON / X coordinate can be up to 179.9999... so would always have one more leading digit than lat.
+         *
+         */
+        int digits = 0;
+        for (char ch : lat.toCharArray()) {
+            if (Character.isDigit(ch)) {
+                digits += 1;
+            }
+        }
+
+        if (digits > 11) {
+            return PrecisionScales.DEFAULT_UNKNOWN_RESOLUTION;
+        }
+
+        return DM_precision_list[digits];
+    }
 
     @Test
     public void precisionScalesFormattingComparison() {
@@ -59,9 +182,9 @@ public class PrecisionScalesTest {
         float F = 13.8888999977771111f;
 
         // 13 decimal places is higher precision than is worth processing:
-        System.out.println("NUM=" + D + "\format2(D,13)=" + PrecisionScales.format2(D, 13));
+        System.out.println("NUM=" + D + "\format2(D,13)=" + format2(D, 13));
         // 11 okay:
-        System.out.println("NUM=" + D + "\tformat2(D,11)=" + PrecisionScales.format2(D, 11));
+        System.out.println("NUM=" + D + "\tformat2(D,11)=" + format2(D, 11));
         System.out.println("NUM=" + F + "\tformat(F, 16)=" + PrecisionScales.format(F, 16));
         System.out.println("NUM=" + D + "\tformat(D, 16)=" + PrecisionScales.format(D, 16));
         System.out.println("NUM=" + D + "\tformat(D, 13)=" + PrecisionScales.format(D, 13));
@@ -70,9 +193,9 @@ public class PrecisionScalesTest {
 
         for (double f : dataD) {
             System.out.println("NUM=" + f
-                    + "\tbd(f,2)=" + PrecisionScales.bigDecimalRounder(f, 2 + 1)
+                    + "\tbd(f,2)=" + bigDecimalRounder(f, 2 + 1)
                     + "\tf(f,2)=" + PrecisionScales.format(f, 2)
-                    + "\tf2(f,1)=" + PrecisionScales.format2(f, 1));
+                    + "\tf2(f,2)=" + format2(f, 2));
         }
         System.out.println("=======Speed tests (ms) =========");
 
@@ -89,7 +212,7 @@ public class PrecisionScalesTest {
 
         t1 = t2;
         for (int x = 0; x < 100000; ++x) {
-            PrecisionScales.format2(F, digits);
+            format2(F, digits);
         }
         t2 = new java.util.Date().getTime();
         System.out.println("Format2 100K: " + (t2 - t1));
@@ -113,7 +236,7 @@ public class PrecisionScalesTest {
 
         t1 = t2;
         for (int x = 0; x < 100000; ++x) {
-            PrecisionScales.bigDecimalRounder(F, digits);
+            bigDecimalRounder(F, digits);
         }
         t2 = new java.util.Date().getTime();
         System.out.println("BD-ROUNDER FORMAT 100K: " + (t2 - t1));
@@ -121,7 +244,7 @@ public class PrecisionScalesTest {
         System.out.println("STRING FMT:   " + String.format("%1.4f", F));
         System.out.println("DECIMAL FMT:  " + PrecisionScales.format(F, digits));
         System.out.println("ROUNDER:      " + PrecisionScales.format(F, digits));
-        System.out.println("BD-ROUNDER:   " + PrecisionScales.bigDecimalRounder(F, digits));
+        System.out.println("BD-ROUNDER:   " + bigDecimalRounder(F, digits));
     }
 
 }
