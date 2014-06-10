aca4bb3541c46701d08ab583b32848b07a5527c8
Marc Ubaldino <ubaldino@mitre.org>
2014-06-10 16:01:25 -0400

    formatting and header review

--- Basics/src/main/java/org/opensextant/util/AnyFilenameFilter.java
+++ Basics/src/main/java/org/opensextant/util/AnyFilenameFilter.java
@@ -1,22 +1,3 @@
-/*
-                                NOTICE
-
-     This software (or technical data) was produced for the U. S.
-  Government under contract W15P7T-08-C-F600, and is subject to the
-  Rights in Data-General Clause 52.227-14 - Alternate IV (June 1987)
-
-         (c) 2007-2008 The MITRE Corporation. All Rights Reserved.
- */
-
-/*
- * AnyFilenameFilter.java
- *
- * Created on October 11, 2007, 4:18 PM
- *
- * To change this template, choose Tools | Template Manager
- * and open the template in the editor.
- */
-
 package org.opensextant.util;
 
 import java.io.*;
