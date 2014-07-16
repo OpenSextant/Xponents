8f638686927cb11f0b42cd948a821fbe1ed800d7
Marc Ubaldino <ubaldino@mitre.org>
2014-07-16 17:05:21 -0400

    XText scripting cleanup

--- XText/script/convert.bat
+++ XText/script/convert.bat
@@ -1,18 +1,7 @@
 set LANG=en_US
 
-#ant -f .\xtext-test.xml  -Dinput=%1%  -Doutput=%2% convert
+echo "Usage -- run as .\script\convert.bat"
 echo JAVA_HOME =  %JAVA_HOME%
-cd ..
+echo See README_convert.txt  for more detail.
 
-REM Add arguments to end of command line:
-REM
-REM   -i  "input dir or file"    Path to input item to convert or crawl
-REM   -o  "output dir"           Path to output folder. It must exist.
-REM   -e                         Conversions of all content will be cached with input
-REM   -x  ".\export\folder"      Export PST or Zip archive contents to the named folder.
-REM   -c                         Children objects will be stored with input
-REM
-REM   "-e" option will override "-o dir".  As "-e" commands output to be organized under -i input dir
-REM   "-c" option will override "-x dir".  As "-c" saves binaries with input; -x exports to external dir.
-REM
-java -Xmx512m  -classpath ".\lib\*" org.opensextant.xtext.XText  -i "%1%" -o "%2%" 
+java -Xmx512m  -classpath ".\lib\*" org.opensextant.xtext.XText  -i %1 -o %2 -x %3
