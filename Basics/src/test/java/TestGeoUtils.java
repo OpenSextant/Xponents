649c717665b17b82c58c6d079af55e5997532731
Marc Ubaldino <ubaldino@mitre.org>
2014-10-31 12:55:59 -0400

    minor tests on util tools

--- Basics/src/test/java/TestGeoUtils.java
+++ Basics/src/test/java/TestGeoUtils.java
@@ -3,6 +3,7 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 import org.opensextant.data.LatLon;
 import org.opensextant.util.GeodeticUtility;
+import org.opensextant.util.GeonamesUtility;
 
 /**
  * 
@@ -14,8 +15,8 @@ import org.opensextant.util.GeodeticUtility;
  */
 public class TestGeoUtils {
 
-    @Test
-    public void test() {
+    //@Test
+    public static void test() {
 
         String test = null;
         // Test parsability
@@ -35,9 +36,19 @@ public class TestGeoUtils {
             LatLon xy = GeodeticUtility.parseLatLon(test);
         } catch (Exception err) {
             //err.printStackTrace();
-            System.out.println("Pass: invalid coordinate, " + test + " fails to parse; ERR=" + err.getMessage());
+            System.out.println("Pass: invalid coordinate, " + test + " fails to parse; ERR="
+                    + err.getMessage());
         }
 
+        try {
+            new GeonamesUtility();
+        } catch (Exception err) {
+            err.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        test();
     }
 
 }
--- Basics/src/test/java/TestTextUtils.java
+++ Basics/src/test/java/TestTextUtils.java
@@ -5,21 +5,28 @@ import org.opensextant.util.TextUtils;
 
 public class TestTextUtils {
 
-	//@Test
-	public static void testRemoveLeft() {
-		int count = TextUtils.removeAnyLeft("-+*ABC", "-").length();
+    //@Test
+    public static void testRemoveLeft() {
+        int count = TextUtils.removeAnyLeft("-+*ABC", "-").length();
 
-		assert (count == 5); // Trim
-								// left
-		count = TextUtils.removeAnyLeft("-+*ABC", "+-").length();
-		assert (count == 4); // Trim left
+        assert (count == 5); // Trim
+                             // left
+        count = TextUtils.removeAnyLeft("-+*ABC", "+-").length();
+        assert (count == 4); // Trim left
 
-		count = TextUtils.removeAny("-+*ABC", "+ - * (^%").length();
-		assert (count == 3); // Remove any chars from string. yields ABC
-	}
+        count = TextUtils.removeAny("-+*ABC", "+ - * (^%").length();
+        assert (count == 3); // Remove any chars from string. yields ABC
+    }
 
-	public static void main(String[] x) {
-		testRemoveLeft();
-	}
+    public static void main(String[] x) {
+        testRemoveLeft();
+
+        for (String l : TextUtils.getLanguageMap().keySet()) {
+            if (l.startsWith("c")) {
+                System.out.println("l=" + l + " name=" + TextUtils.getLanguage(l));
+            }
+        }
+        System.out.println("Chinese? "+TextUtils.getLanguage("chi"));
+    }
 
 }
