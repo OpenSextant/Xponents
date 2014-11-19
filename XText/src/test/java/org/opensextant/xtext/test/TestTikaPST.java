d094ffa225961186e2c79f7562f94991e8c3ce06
Marc Ubaldino <ubaldino@mitre.org>
2014-11-19 13:02:57 -0500

    Tika PST test

---
+++ XText/src/test/java/org/opensextant/xtext/test/TestTikaPST.java
@@ -0,0 +1,39 @@
+package org.opensextant.xtext.test;
+
+import java.io.IOException;
+
+import org.opensextant.ConfigException;
+import org.opensextant.xtext.XText;
+
+public class TestTikaPST {
+
+    
+    /** Compare Tika's PST conversion to XText non-Tika PST conversion.
+     */
+    public static main(String args[]) {
+
+        String input = args[0]; // Path to a PST.  NOTE, java-libpst provides some good test data.
+
+        try {
+            XText xt = new XText();
+            xt.enableSaving(true);
+            xt.enableTikaPST(true);
+            xt.getPathManager().enableSaveWithInput(false); // creates a ./text/ Folder locally in directory.
+            xt.clearSettings();
+            xt.convertFileType("pst");
+
+            xt.getPathManager().setConversionCache("./xtext-testing");
+            xt.setup();
+            xt.extractText(input);
+        } catch (IOException ioerr) {
+            ioerr.printStackTrace();
+            fail("IO issue" + ioerr.getMessage());
+
+        } catch (ConfigException cfgerr) {
+            cfgerr.printStackTrace();
+            fail("Config issue" + cfgerr.getMessage());
+
+        }
+    }
+
+}
