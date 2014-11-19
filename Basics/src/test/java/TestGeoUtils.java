5c500df18ae070c55af76a5e0bca7b43e2eb80cc
Marc Ubaldino <ubaldino@mitre.org>
2014-11-19 12:51:37 -0500

    Junit tests for some tools

--- Basics/src/test/java/TestGeoUtils.java
+++ Basics/src/test/java/TestGeoUtils.java
@@ -1,13 +1,12 @@
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import java.io.IOException;
+import org.opensextant.data.GeoBase;
 import org.opensextant.data.LatLon;
+import org.opensextant.data.Place;
 import org.opensextant.util.GeodeticUtility;
 import org.opensextant.util.GeonamesUtility;
-
-/**
- * 
- */
 
 /**
  * @author ubaldino
@@ -15,8 +14,13 @@ import org.opensextant.util.GeonamesUtility;
  */
 public class TestGeoUtils {
 
-    //@Test
-    public static void test() {
+   @Test
+   public void testResources() throws IOException{
+       new GeonamesUtility();
+   }
+
+   @Test
+    public void testGeodetics() {
 
         String test = null;
         // Test parsability
@@ -25,6 +29,26 @@ public class TestGeoUtils {
             LatLon xy = GeodeticUtility.parseLatLon(test);
             test = "-34.0, 78.9";
             xy = GeodeticUtility.parseLatLon(test);
+
+            LatLon p2 = new Place(-35.2, 79.0);
+            LatLon p3 = new Place(-35.2, -101.0);
+            System.out.println("METERS from point to point " + xy + " to " + p2 + " = "
+                    + GeodeticUtility.distanceMeters(xy, p2));
+            System.out.println("METERS from point to point " + p2 + " to " + p3 + " = "
+                    + GeodeticUtility.distanceMeters(p2, p3));
+            System.out.println("METERS from point to point " + p2 + " to " + p3 + " = "
+                    + GeodeticUtility.distanceMeters(p3, p2));
+            System.out.println("METERS from point to point " + p2 + " to " + p3 + " = "
+                    + GeodeticUtility.distanceMeters(p3, p3));
+            
+            // Great circle in meters...
+            LatLon eq1 = new Place(0, 79.0);
+            LatLon eq2 = new Place(0, -101.0);
+            System.out.println("METERS from point to point " + eq1 + " to " + eq2 + " = "
+                    + GeodeticUtility.distanceMeters(eq1, eq2));
+            
+            assert(true);
+
         } catch (Exception err) {
             fail("Could not parse");
             err.printStackTrace();
@@ -38,17 +62,8 @@ public class TestGeoUtils {
             //err.printStackTrace();
             System.out.println("Pass: invalid coordinate, " + test + " fails to parse; ERR="
                     + err.getMessage());
-        }
 
-        try {
-            new GeonamesUtility();
-        } catch (Exception err) {
-            err.printStackTrace();
+            assert(true);
         }
     }
-
-    public static void main(String[] args) {
-        test();
-    }
-
 }
--- Basics/src/test/java/TestTextUtils.java
+++ Basics/src/test/java/TestTextUtils.java
@@ -5,8 +5,8 @@ import org.opensextant.util.TextUtils;
 
 public class TestTextUtils {
 
-    //@Test
-    public static void testRemoveLeft() {
+    @Test
+    public void testRemoveLeft() {
         int count = TextUtils.removeAnyLeft("-+*ABC", "-").length();
 
         assert (count == 5); // Trim
@@ -18,18 +18,25 @@ public class TestTextUtils {
         assert (count == 3); // Remove any chars from string. yields ABC
     }
 
-    public static void main(String[] x) {
-        testRemoveLeft();
+    @Test
+    public void testScriptDetection() {
 
-        for (String l : TextUtils.getLanguageMap().keySet()) {
-            if (l.startsWith("c")) {
-                System.out.println("l=" + l + " name=" + TextUtils.getLanguage(l));
-            }
+        assert(TextUtils.isLatin("Ö"));
+        assert(TextUtils.isLatin("a Ö 5 !"));
+        assert(!TextUtils.isLatin("a Ö 杨寨 5 !"));
+
+        String t = TextUtils.replaceDiacritics("a Ö ø Ø é å Å 杨寨 5 ! ē M ē ā");
+        if (!t.equals("a O o O e a A 杨寨 5 ! e M e a")) {
+            fail("Diacritics not replaced!");
         }
-        System.out.println("Chinese? "+TextUtils.getLanguage("chi"));
-        System.out.println("French(fre)? "+TextUtils.getLanguage("fre"));
-        System.out.println("French(fra)? "+TextUtils.getLanguage("fra"));
-        System.out.println("French(french)? "+TextUtils.getLanguage("FRENCH"));
+    }
+
+    @Test
+    public void testLanguageCodes() {
+        assert ("Chinese".equals(TextUtils.getLanguage("chi").getName()));
+        assert ("French".equals(TextUtils.getLanguage("fre").getName()));
+        assert ("French".equals(TextUtils.getLanguage("fra").getName()));
+        assert ("French".equals(TextUtils.getLanguage("FRENCH").getName()));
     }
 
 }
