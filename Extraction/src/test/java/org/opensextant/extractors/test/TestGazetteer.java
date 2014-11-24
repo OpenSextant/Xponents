e1f48616c2d641e958ae5d5d613eac31acc864d8
Marc Ubaldino <ubaldino@mitre.org>
2014-11-24 09:59:09 -0500

    List country metadata from Gazetteer utils

--- Extraction/src/test/java/org/opensextant/extractors/test/TestGazetteer.java
+++ Extraction/src/test/java/org/opensextant/extractors/test/TestGazetteer.java
@@ -1,12 +1,20 @@
 package org.opensextant.extractors.test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
 
 import org.opensextant.data.Country;
 import org.opensextant.data.Place;
 import org.opensextant.extractors.geo.SolrGazetteer;
 import org.opensextant.util.GeonamesUtility;
+import org.opensextant.util.TextUtils;
 
 public class TestGazetteer {
 
@@ -41,11 +49,37 @@ public class TestGazetteer {
             /* A Solr/lucene style parametric search.  By default, search queries "name" field.
              *
              */
+            /*
             List<Place> matches = gaz.search("+Boston +City");
-
             for (Place pc : matches) {
                 System.out.println(pc.toString() + " which is categorized as: "
                         + geodataUtil.getFeatureName(pc.getFeatureClass(), pc.getFeatureCode()));
+            }
+            */
+
+            List<String> cnames = new ArrayList<>();
+            Map<String, Boolean> done = new TreeMap<>();
+            for (Country C : geodataUtil.getCountries().values()) {
+                String q = String.format("name:%s AND -feat_code:PCLI AND -feat_code:TERR",
+                        C.getName());
+                List<Place> country_name_matches = gaz.search(q, true);
+                //System.out.println("Matched names for " + C.getName() + ":\t");
+                String cname = TextUtils.removeDiacritics(C.getName()).toLowerCase();
+
+                done.put(cname, false);
+                for (Place p : country_name_matches) {
+                    String pname = TextUtils.removeDiacritics(p.getName());
+                    if (pname.equalsIgnoreCase(cname)) {
+                        done.put(p.getName().toLowerCase(), true);
+                    }
+                }
+
+                cnames.add(cname);
+            }
+
+            //Collections.sort(cnames);
+            for (String cname : done.keySet()) {
+                System.out.println(String.format("\"%s\",", cname) + !done.get(cname));
             }
 
         } catch (Exception err) {
