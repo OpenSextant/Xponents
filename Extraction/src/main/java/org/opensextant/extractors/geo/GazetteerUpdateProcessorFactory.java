59c85cd31241442d10f7089701d9e4c264658a28
Marc Ubaldino <ubaldino@mitre.org>
2015-03-30 16:13:58 -0400

    copyright year

--- Extraction/src/main/java/org/opensextant/extractors/geo/GazetteerUpdateProcessorFactory.java
+++ Extraction/src/main/java/org/opensextant/extractors/geo/GazetteerUpdateProcessorFactory.java
@@ -1,3 +1,18 @@
+/**
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ *               http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ *    Copyright 2013-2015 The MITRE Corporation.
+ */
 package org.opensextant.extractors.geo;
 
 import java.io.IOException;
@@ -63,6 +78,10 @@ public class GazetteerUpdateProcessorFactory extends UpdateRequestProcessorFacto
         try {
             stopTerms = GazetteerMatcher.loadExclusions(GazetteerMatcher.class
                     .getResource("/filters/non-placenames.csv"));
+            if (stopTerms == null) {
+                logger.error("Init failure:  Did not find 'non-placenames' file in CLASSPATH."
+                        + "\nThis will not work properly with out such data.");
+            }
 
             helper = new GeonamesUtility();
         } catch (Exception err) {
