afe136977877237e54578b647807551f9ecd734a
Marc Ubaldino <ubaldino@mitre.org>
2015-03-30 15:18:24 -0400

    Deprecated - JS version of script is not able to process entries in more complex ways; Java UpdateRequestProcessor is superior to this

--- solr/gazetteer/conf/add-gazetteer.js
+++ solr/gazetteer/conf/add-gazetteer.js
@@ -6,30 +6,42 @@ See http://wiki.apache.org/solr/ScriptUpdateProcessor for more details.
 */
 
 var includeCategorySet = null;
+var includeAll = false;
 {
     var ic = params.get("include_category");//array of String
     if (ic != null) {
         includeCategorySet = new java.util.HashSet(ic);
+        if (includeCategorySet.contains('all')){
+            includeAll = true;
+        }
     }
+    logger.info("add-gaz JS#startup: 'Add Gazetteer'; Params: Include=" + includeCategorySet);
 }
 var CAT_FIELD = params.get("category_field") || "SplitCategory";
+var lat, lon;
+
 
 function processAdd(cmd) {
 
     var doc = cmd.solrDoc;  // org.apache.solr.common.SolrInputDocument
-    // logger.info("update-script#processAdd: id=" + id);
+    // logger.info("update-script#processAdd: id="+ doc.getFieldValue("id"));
 
 
     /* See solrconfig for documentation on gazetteer filtering
     * =======================================================
     */
-
-    if (includeCategorySet != null) {
+    if (!includeAll && includeCategorySet != null) {
         var cat = doc.getFieldValue(CAT_FIELD) || "general";
         if (!includeCategorySet.contains(cat)) {
-            logger.trace("update-script EXCLUDE: {}", doc);
+            logger.info("add-gaz JS# EXCLUDE: " + doc);
             return false;
         }
+    }
+ 
+    var nm = doc.getFieldValue("name");
+    if (nm.length() < 2){
+        doc.setField("search_only", "true");
+        logger.info("add-gaz JS#processAdd: Short name set search only");
     }
 
     /* End Filtering
