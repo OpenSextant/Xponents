ac90279ea0ca27a67314ece7e80a69beec6cd401
Marc Ubaldino <ubaldino@mitre.org>
2015-05-11 13:58:09 -0400

    Testing resources

--- Extraction/src/test/java/org/opensextant/extractors/test/TestGazMatcher.java
+++ Extraction/src/test/java/org/opensextant/extractors/test/TestGazMatcher.java
@@ -1,21 +1,73 @@
 package org.opensextant.extractors.test;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
 
 import org.opensextant.extraction.MatchFilter;
+import org.opensextant.extraction.TextMatch;
 import org.opensextant.extractors.geo.GazetteerMatcher;
 import org.opensextant.extractors.geo.PlaceCandidate;
 import org.opensextant.util.FileUtility;
 
+/**
+ * Setup for testing:
+ * 
+ * - build Gazeteer project
+ * - load data into ./solr/gazetteer solr core
+ * - run this test main,
+ *    -Dopensextant.solr=<path to gazetteer solr home>
+ *    one argument, then input file to test.
+ *    
+ * @author ubaldino
+ *
+ */
 public class TestGazMatcher {
 
-    public static void printGeoTags(List<PlaceCandidate> list) {
-        for (PlaceCandidate pc : list) {
+    public static void printTextTags(List<TextMatch> list) {
+        for (TextMatch pc : list) {
             System.out.println("Name:" + pc.getText());
         }
+    }
+
+    public static void printGeoTags(List<PlaceCandidate> list) {
+        for (TextMatch pc : list) {
+            System.out.println("Name:" + pc.getText());
+        }
+    }
+
+    public static void printGeoTags(TextMatch match) {
+        System.out.println("Name:" + match.getText());
+    }
+
+    public static void summarizeFindings(List<TextMatch> matches) {
+        Set<String> placeNames = new TreeSet<>();
+        Set<String> countryNames = new TreeSet<>();
+        System.out.println("MENTIONS ALL == " + matches.size());
+        for (TextMatch tm : matches) {
+            printGeoTags(tm);
+            if (tm instanceof PlaceCandidate) {
+                PlaceCandidate p = (PlaceCandidate) tm;
+                if (p.isCountry) {
+                    countryNames.add(p.getText());
+                } else {
+                    placeNames.add(p.getText());
+                }
+            }
+        }
+
+        System.out.println("MENTIONS DISTINCT == " + placeNames.size());
+        System.out.println(placeNames);
+        System.out.println("MENTIONS COUNTRY == " + countryNames.size());
+        System.out.println(countryNames);
+    }
+
+    public static List<TextMatch> copyFrom(List<PlaceCandidate> list) {
+        List<TextMatch> list2 = new ArrayList<>();
+        list2.addAll(list);
+        return list2;
     }
 
     /**
@@ -47,26 +99,14 @@ public class TestGazMatcher {
             System.out.println(docContent);
 
             List<PlaceCandidate> matches = sm.tagText(docContent, "main-test");
-            printGeoTags(matches);
+            for (PlaceCandidate pc : matches) {
+                printGeoTags(pc);
+            }
 
             String buf = FileUtility.readFile(args[0]);
             matches = sm.tagText(buf, "main-test", true);
-            Set<String> placeNames = new TreeSet<>();
-            Set<String> countryNames = new TreeSet<>();
-            for (PlaceCandidate p : matches) {
-                if (p.isCountry) {
-                    countryNames.add(p.getText());
-                } else {
-                    placeNames.add(p.getText());
-                }
-            }
 
-            System.out.println("MENTIONS ALL == " + matches.size());
-            printGeoTags(matches);
-            System.out.println("MENTIONS DISTINCT == " + placeNames.size());
-            System.out.println(placeNames);
-            System.out.println("MENTIONS COUNTRY == " + countryNames.size());
-            System.out.println(countryNames);
+            summarizeFindings(copyFrom(matches));
         } catch (Exception err) {
             err.printStackTrace();
         } finally {
---
+++ Extraction/src/test/java/org/opensextant/extractors/test/TestPlaceGeocoder.java
@@ -0,0 +1,40 @@
+package org.opensextant.extractors.test;
+
+import java.io.File;
+import java.util.List;
+
+import org.opensextant.extraction.TextMatch;
+import org.opensextant.extractors.geo.PlaceGeocoder;
+import org.opensextant.util.FileUtility;
+
+public class TestPlaceGeocoder extends TestGazMatcher {
+
+    /**
+     * see TestGazMatcher documentation
+     *  
+     * @param args
+     */
+    public static void main(String[] args) {
+        try {
+            // INIT once.
+            PlaceGeocoder geocoder = new PlaceGeocoder();
+            geocoder.configure();
+
+            // Call as many times as you have documents...
+            //
+            try {
+                List<TextMatch> matches = geocoder.extract(FileUtility.readFile(new File(args[0]),
+                        "UTF-8"));
+                summarizeFindings(matches);
+            } catch (Exception procErr) {
+                procErr.printStackTrace();
+            }
+
+            // CALL only when you are done for good.
+            geocoder.cleanup();
+
+        } catch (Exception err) {
+            err.printStackTrace();
+        }
+    }
+}
