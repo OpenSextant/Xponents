9971096194247b67f9b7c5207d37316fb9d8963c
Marc Ubaldino <ubaldino@mitre.org>
2015-10-08 20:42:22 -0400

    minor gazetteer items

--- solr/build.sh
+++ solr/build.sh
@@ -1,4 +1,18 @@
 if [ ! -d ./log ] ;  then
   mkdir log
 fi
-ant -f build-gazetteer.xml _index > log/build.log & 
+ant -f build-gazetteer.xml _index > log/build.log  
+
+SERVER=localhost:7000
+
+# Finally add adhoc entries from JSON formatted files.
+#
+# Yes, this depends on curl. Could re-implement as Ant call.
+# This could be: http://ant-contrib.sourceforge.net/tasks/tasks/post_task.html
+#
+curl --noproxy localhost  "http://$SERVER/solr/gazetteer/update?commit=true" \
+   -H Content-type:application/json --data-binary @./adhoc-US-city-nicknames.json
+
+
+# When done for the day,  optimize
+curl --noproxy localhost "http://$SERVER/solr/gazetteer/update?stream.body=<optimize/>"
