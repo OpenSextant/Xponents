069ec48f969c8fd8473d95be702c4672d4cf1003
Marc Ubaldino <ubaldino@mitre.org>
2015-12-04 17:10:24 -0500

    Renamed so this observer can see any hard location, not just coordinate patterns.

---
+++ Extraction/src/main/java/org/opensextant/extractors/geo/LocationObserver.java
@@ -0,0 +1,28 @@
+package org.opensextant.extractors.geo;
+
+import org.opensextant.data.Geocoding;
+import org.opensextant.data.Place;
+
+/**
+ * Apply this interface where application logic observes a coordinate or any hard location reference.
+ * 
+ * @author ubaldino
+ *
+ */
+public interface LocationObserver {
+    /**
+     * If a given geo is in scope, fire this event.
+     * 
+     * @param geo
+     */
+    public void locationInScope(Geocoding geo);
+    
+    /**
+     * The place know by the ID, p.getKey() or p.getPlaceID()
+     * was it observed directly or indirectly in this document?
+     * 
+     * @param p
+     * @return
+     */
+    public boolean placeObserved(Place p);
+}
