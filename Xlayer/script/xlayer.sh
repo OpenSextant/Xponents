5c25bbde04437523a028ed139b29df6ba4fd18c2
Marc Ubaldino <ubaldino@mitre.org>
2016-08-25 10:11:42 -0400

    dup mem args

--- Xlayer/script/xlayer.sh
+++ Xlayer/script/xlayer.sh
@@ -8,7 +8,7 @@ logfile=$basedir/log/stderr.log
 stdout=$basedir/log/xlayer.log
 XPONENTS_SOLR=/mitre/xponents-solr
 
-nohup java -Dopensextant.solr=$XPONENTS_SOLR -Xmx2g  -Djava.util.logging.config.file=./etc/logging.properties -Xmx512m  \
+nohup java -Dopensextant.solr=$XPONENTS_SOLR -Xmx2g  -Djava.util.logging.config.file=./etc/logging.properties  \
    -classpath "$basedir/etc:$XPONENTS_SOLR/gazetteer/conf:$basedir/lib/*" org.opensextant.xlayer.server.XlayerServer   $*  2>$logfile > $stdout &
 
 
