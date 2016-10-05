05888b870c9bd493939b35dff8169c558de88024
Marc Ubaldino <ubaldino@mitre.org>
2016-10-05 16:47:41 -0400

    python work

--- Extraction/src/main/python/opensextant/Data.py
+++ Extraction/src/main/python/opensextant/Data.py
@@ -2,25 +2,25 @@
 Xponents Python Data: see comparable classes in Xponents Basics data pkg in Java.
 '''
 class Country:
-   '''  Country metadata
-   '''
-   def __init__(self):
-       self.cc_iso2 = None
-       self.cc_iso3 = None
-       self.cc_fips = None
-       self.name = None
-       self.namenorm = None
-       self.aliases = []
-       self.is_territory = False
-       self.is_unique_name = False
-       self.timezones = []
-       self.languages = set([])
-       self.primary_language = None
-       self.lat = 0
-       self.lon = 0
+    '''  Country metadata
+    '''
+    def __init__(self):
+        self.cc_iso2 = None
+        self.cc_iso3 = None
+        self.cc_fips = None
+        self.name = None
+        self.namenorm = None
+        self.aliases = []
+        self.is_territory = False
+        self.is_unique_name = False
+        self.timezones = []
+        self.languages = set([])
+        self.primary_language = None
+        self.lat = 0
+        self.lon = 0
 
-   def __str__(self):
-       return u'%s (%s)' %(self.name, self.cc_iso2)
+    def __str__(self):
+        return u'%s (%s)' %(self.name, self.cc_iso2)
 
 
 def validate_lat(f):
@@ -68,3 +68,68 @@ class Coordinate:
         else:
             return 'unset'
 
+
+class Place(Coordinate):
+    '''
+    Location or GeoBase
+    + Coordinate
+    + Place
+    + Country
+    
+    or 
+    Location
+    + Coordinate
+       + Place
+       
+       etc.  Not sure of the best data model for inheritance.
+    This Python API hopes to simplify the concepts in the Java API.
+    
+    '''
+    def __init__(self, pid, name, lat=None, lon=None):
+        Coordinate.__init__(self, None, lat=lat, lon=lon)
+        self.place_id = pid
+        self.name = name
+
+        self.is_ascii = False
+        self.is_upper = False
+        self.adm1_postalcode = None # Province Postal CODE?
+        self.place_postalcode = None # ZIP CODE?
+        self.name_type = None
+        self.country = None
+        self.country_code = None
+        self.feature_class = None
+        self.feature_code = None
+        self.adm1 = None
+        self.adm1_name = None
+        self.adm2 = None
+        self.adm2_name = None
+        self.source = None
+        self.name_bias = 0.0
+        self.id_bias = 0.0
+        self.precision= -1
+        self.method = None;
+        self.population = 0
+        self.hierachical_path= None
+
+    def has_coordinate(self):
+        if self.validate():
+            return (self.Y != 0 and self.X != 0)
+        return False
+    
+    def get_location(self):
+        ''' Returns (LAT, LON) tuple
+        @return: tuple, (lat,lon)
+        '''
+        return (self.Y, self.X)
+        
+    def set_location(self, lat, lon):
+        self.set(lat, lon)
+
+    def __str__(self):
+        crd = 'Unset'
+        if self.Y:
+            crd = '%3.4f, %3.4f'  % (self.Y, self.X)
+        meta = '%s (%s), %s'  %( self.name, self.place_id, self.country_code)
+        return ', '.join([meta, crd])
+        
+    
--- Extraction/src/main/python/test/test_data.py
+++ Extraction/src/main/python/test/test_data.py
@@ -1,4 +1,4 @@
-from opensextant.Data import Coordinate
+from opensextant.Data import Coordinate, Place
 
 
 yx = Coordinate({'lat':'55.5', 'lon':'111'})
@@ -8,3 +8,7 @@ print yx, " is valid? ", yx.validate()
 yx = Coordinate({'lat':'55.5', 'lon':'-181'})
 print yx, " is valid? ", yx.validate()
 
+pl = Place('nowhere', 'Nowhere Really', lat='55.5', lon='-181')
+pl.adm1 = 'OBS'
+pl.adm1_name = 'State of Obsoletion'
+print str(pl)
