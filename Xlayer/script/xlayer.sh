c6898ee7a96fdb08e398fcf76ed7c16e935b40ab
Marc Ubaldino <ubaldino@mitre.org>
2016-10-26 21:58:16 -0400

    testing

--- Xlayer/script/xlayer.sh
+++ Xlayer/script/xlayer.sh
@@ -8,7 +8,8 @@ logfile=$basedir/log/stderr.log
 stdout=$basedir/log/xlayer.log
 XPONENTS_SOLR=/mitre/xponents-solr
 
-nohup java -Dopensextant.solr=$XPONENTS_SOLR -Xmx2g  -Djava.util.logging.config.file=./etc/logging.properties  \
+#-Djava.util.logging.config.file=./etc/logging.properties  \
+nohup java -Dopensextant.solr=$XPONENTS_SOLR -Xmx2g   -Dlogback.configurationFile=$basedir/etc/logback.xml \
    -classpath "$basedir/etc:$XPONENTS_SOLR/gazetteer/conf:$basedir/lib/*" org.opensextant.xlayer.server.XlayerServer   $*  2>$logfile > $stdout &
 
 
