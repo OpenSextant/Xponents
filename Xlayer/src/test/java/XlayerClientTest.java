b563c2f6cfdc5910b7d2907b445aa2b677ddda81
Marc Ubaldino <ubaldino@mitre.org>
2017-05-30 12:56:43 -0400

    added comments; changed version num

--- Xlayer/src/test/java/XlayerClientTest.java
+++ Xlayer/src/test/java/XlayerClientTest.java
@@ -13,10 +13,29 @@ public class XlayerClientTest {
 		URL url;
 		try {
 			url = new URL(args[0]);
+			
+			/*
+			 * Create client.
+			 */
 			XlayerClient c = new XlayerClient(url);
 			try {
+			    /* 
+			     * Prepare request.  Text must be UTF-8 encoded.
+			     * Note -- readFile() here assumes the file is unicode content
+			     * 
+			     */
 				String text = FileUtility.readFile(args[1]);
 				String docid = args[1];
+				
+				/*
+				 * Process the text and print results to console.
+				 * Result is an array of TextMatch objects.  For each particular
+				 * TextMatch (Xponents Basic API), you have some common fields related to the 
+				 * text found, and then class-specific fields and objects you need to evaluate yourself.
+				 * 
+				 * The XlayerClient process() method makes use of Transforms helper class to 
+				 * digest JSON annotations into Java API TextMatch objects of various flavors.
+				 */
 				List<TextMatch> results = c.process(docid, text);
 				for (TextMatch m : results) {
 					System.out.println(String.format("Found %s %s @ (%d:%d)", m.getType(), m.getText(), m.start, m.end));
