f6bbf288ed562c04aa3bf7ae6b4ca970d5558b9e
Marc Ubaldino <ubaldino@mitre.org>
2018-07-06 15:41:40 -0400

    logging setup

--- Examples/script/crawl.sh
+++ Examples/script/crawl.sh
@@ -3,14 +3,12 @@ export LANG=en_US
 script=`dirname $0;`
 basedir=`cd -P $script/..; echo $PWD`
 
-#echo "Usage -- run as ./script/convert.sh"
-# echo JAVA_HOME =  $JAVA_HOME
 echo "Some cautionary points:"
 echo "If using proxy, set http_proxy, default port is 80"  
 echo "If link you are using is a folder, you must add trailing slash, eg., -l http://a.b.com/folder/ " 
 
 echo $*
-logfile=$basedir/logs/xtext-stderr.log
+logfile=$basedir/log/xtext-stderr.log
 # Get proxy from http_proxy
 proxyHost=`echo $http_proxy | sed -e "s:http\://::g;" | sed -e "s:\:.$::;"`
 
@@ -18,6 +16,7 @@ WEBSITE=$1
 OUTPUT=$2
 # Omit proxy argument if you don't use a proxy/firewall.
 
-java  -Dlog4j.configuration="file:${basedir}/etc/log4j.properties" \
-   -Dhttp.proxyHost=${proxyHost} -Dhttp.proxyPort=80 -Dxtext.home="${basedir}" -Xmx512m  \
+OPTS="-Dlogback.configurationFile=${basedir}/etc/logback.xml"
+PROXY="-Dhttp.proxyHost=${proxyHost} -Dhttp.proxyPort=80"
+java $OPTS $PROXY  -Dxtext.home="${basedir}" -Xmx512m  \
    -classpath "$basedir/lib/*" org.opensextant.examples.WebCrawl -l $WEBSITE -o $OUTPUT -d
