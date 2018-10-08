ffba3e768195c9af226bbec4dd8ba7cfa2a747f7
Marc Ubaldino <ubaldino@mitre.org>
2018-10-07 22:44:48 -0400

    rearrange stuff -- migrate to all groovy scripting.

---
+++ Examples/script/Xponents.groovy
@@ -0,0 +1,91 @@
+
+import org.opensextant.examples.*
+import org.opensextant.extractors.test.*
+
+static void usage(){
+  println "Xponents <TEST> <ARGS....>"
+  println "  each TEST has different contextual ARGS for command line usage."
+  println "  these are all demonstrational tests."
+
+  println "Xponents <TEST> help"
+  menu()
+  System.exit(-1)
+}
+
+static void menu(){  
+
+   println '''Tests available for basic tests or adhoc interaction 
+   ======================
+      gazetteer  -- Gazetteer queries and experiments.
+      xcoord     -- XCoord coordinate extraction tests
+      geocoder   -- Geotagging and Geocoding
+      geotemp    -- Geotagging and temporal extraction
+      poli       -- Pattern-based extraction
+      xtemp      -- Temporal extraction tests
+      xtext      -- XText demonstration - crawl and convert (files to text) local folders
+      web-crawl  -- Content crawl and convert. Advanced XText demo, e.g., feed convert and extraction pipeline.
+      =======================
+     '''
+}
+
+
+static void main(String[] args){
+
+  def test 
+  def app
+  if (args.length < 2) {
+    usage()
+  }
+  test = args[0]
+
+  switch(test) {
+      case 'gazetteer':
+        app = new XponentsGazetteerQuery()
+        //  --lookup "San Francisco, CA, USA"  
+        break;
+      case 'xcoord':
+        app = new TestXCoord()
+         
+        //-u input
+        //-f tests
+        //-t input-lines
+        break;
+      
+      case 'geocoder':
+        app = new TestPlaceGeocoder()
+        // -i input -o output
+        break;
+      case 'geotemp':
+        app = new BasicGeoTemporalProcessing()
+        // -i input -o output -f fmt
+        break;
+      case 'xtext':
+        app = new org.opensextant.xtext.XText()
+        // -i input -o output -e -h
+        break;
+      case 'poli':
+        app = new TestPoLi()
+        // -c cfg -u userinput
+        // or 
+        // -f 
+        break;    
+      case 'xtemp':
+        app = new TestXTemporal()
+        // -f 
+        // OR 
+        // file
+        break;
+      case 'web-crawl':
+        app = new org.opensextant.examples.WebCrawl()
+        // -l $WEBSITE -o $OUTPUT -d
+        break;
+        
+      case 'social-geo':
+        app = new SocialDataApp()
+        break;
+      default:
+        usage();
+  }
+  args_len=args.length-1
+  app.main(args[1..args_len] as String[])
+}
---
+++ Examples/script/xponents-demo.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+scripts=`dirname $0`
+XP=`cd -P $scripts/..; echo $PWD`
+
+export CLASSPATH=$XP/etc:$XP/etc/xponents-gazetteer-meta.jar:$XP/lib/*:$XP/script/*
+
+SOLR_HOME=$XP/xponents-solr/solr7
+if [ -d $XP/solr/ ] ; then
+    # Use dir in development source tree.
+    SOLR_HOME=$XP/solr/solr7
+fi
+
+proxy_args=
+if [ -n "$http_proxy" -o -n "$https_proxy" ]; then 
+    proxyHost=`echo $http_proxy | sed -e "s:http\://::g;" | sed -e "s:\:.$::;"`
+    proxy_args="-Dhttp.proxyHost=${proxyHost} -Dhttp.proxyPort=80"
+    proxy_args="-Dhttps.proxyHost=${proxyHost} -Dhttps.proxyPort=80 $PROXY"
+fi
+
+xponents_args=" -Dopensextant.solr=$SOLR_HOME -Xmx1200m -Xms1200m "
+logging_args="-Dlogback.configurationFile=$XP/etc/logback.xml"
+tika_args="-Dtika.config=$XP/etc/tika-config.xml"
+
+java  $xponents_args $logging_args $tika_args $proxy_args -cp $CLASSPATH \
+  org.codehaus.groovy.tools.GroovyStarter --main groovy.ui.GroovyMain \
+  $XP/script/Xponents.groovy  "$@"
