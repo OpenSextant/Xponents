7a4badad4b110fe6deef58a0516dca7665765bf4
Marc Ubaldino <ubaldino@mitre.org>
2021-03-25 05:37:29 -0400

    Improved, more flexible formatters

---
+++ Core/src/main/java/org/opensextant/output/AbstractGenericFormatter.java
@@ -0,0 +1,223 @@
+package org.opensextant.output;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.io.FileUtils;
+import org.opensextant.ConfigException;
+import org.opensextant.processing.Parameters;
+import org.opensextant.processing.ProcessingException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Abstract class encapsulating basic results formatter functionality without
+ * prescribing schema
+ *
+ * @author Marc Ubaldino, MITRE Corp.
+ */
+abstract public class AbstractGenericFormatter implements ResultsFormatter {
+
+    /** The output params. */
+    protected Parameters outputParams = null;
+
+    /** The overwrite. */
+    public boolean overwrite = false;
+
+    /**
+     * Sets the parameters.
+     *
+     * @param params job parameters
+     */
+    @Override
+    public void setParameters(Parameters params) {
+        outputParams = params;
+    }
+
+    protected final Logger log = LoggerFactory.getLogger(getClass());
+
+    /** The filename. */
+    private String filename = "unset";
+
+    /** File extension for callers to know. */
+    public String outputExtension = null;
+
+    /** reflected by extension; an enum in OpenSextant */
+    protected String outputType = null;
+
+    /** Size of text window around matches -- to use as excerpts */
+    protected static int TEXT_WIDTH = 150;
+
+    public boolean debug = false;
+
+    /** Distinct set of fields in your output schema. */
+    protected HashSet<String> fieldSet = new HashSet<String>();
+
+    /**
+     * Adds the field.
+     *
+     * @param f field name
+     * @throws ConfigException
+     */
+    @Override
+    public void addField(String f) throws ConfigException {
+        fieldOrder.add(f);
+        fieldSet.add(f);
+    }
+
+    /**
+     * Removes the field.
+     *
+     * @param f field name
+     * @throws ConfigException
+     */
+    @Override
+    public void removeField(String f) throws ConfigException {
+        fieldOrder.remove(f);
+        fieldSet.remove(f);
+    }
+
+    /** The field_order. */
+    protected List<String> fieldOrder = new ArrayList<String>();
+
+    /**
+     * Default fields for generic CSV output. If GIS output is desired, then use
+     * GeoCSV formatter.
+     */
+    protected final void defaultFields() {
+
+    }
+
+    /**
+     * A basic job name that reflects file name.
+     *
+     * @return the job name
+     */
+    @Override
+    public String getJobName() {
+        return this.outputParams.getJobName();
+    }
+
+    /**
+     * Sets the output filename.
+     *
+     * @param fname the new output filename
+     */
+    @Override
+    public void setOutputFilename(String fname) {
+        this.filename = fname;
+    }
+
+    /**
+     * Sets the output dir.
+     *
+     * @param path the new output dir
+     */
+    @Override
+    public void setOutputDir(String path) {
+
+        // Create the directory if necessary
+        File resultsDir = new File(path);
+        if (!resultsDir.exists()) {
+            resultsDir.mkdir();
+        }
+
+        this.outputParams.outputDir = resultsDir.getPath();
+    }
+
+    /**
+     * Gets the output filepath.
+     *
+     * @return the output filepath
+     */
+    @Override
+    public String getOutputFilepath() {
+        return this.outputParams.outputDir + File.separator + this.filename;
+    }
+
+    /**
+     * Creates the output file name.
+     *
+     * @return the string
+     */
+    protected String createOutputFileName() {
+        return this.filename + this.outputExtension;
+    }
+
+    /**
+     * Gets the output type.
+     *
+     * @return the output type
+     */
+    @Override
+    public String getOutputType() {
+        return this.outputType;
+    }
+
+    /**
+     * This is checked only by internal classes as they create output streams.
+     *
+     * @param previous_run file path of previous output
+     */
+    protected void deleteOutput(File prevOutput) {
+        if (prevOutput.exists()) {
+            FileUtils.deleteQuietly(prevOutput);
+        }
+    }
+
+    /**
+     * uniform helper for overwrite check.
+     *
+     * @param item
+     *             the item
+     * @throws ProcessingException if unable to overwrite file
+     */
+    protected void checkOverwrite(File item) throws ProcessingException {
+        if (this.overwrite) {
+            this.deleteOutput(item);
+        } else if (item.exists()) {
+            throw new ProcessingException("Overwrite not enabled FILE=" + item.getPath() + " exists");
+        }
+    }
+
+    /**
+     * Start.
+     *
+     * @param nm output file name/worksheet name
+     * @throws ProcessingException
+     */
+    @Override
+    public abstract void start(String nm) throws ProcessingException;
+
+    /**
+     * Write the data to the output stream.
+     *
+     * @param values Map of data
+     */
+    public abstract void writeRow(Map<String, Object> values);
+
+    /**
+     * Finish.
+     */
+    @Override
+    public abstract void finish();
+
+    /**
+     * Create the output stream appropriate for the output type. IO is created
+     * using the filename represented by getOutputFilepath()
+     *
+     * @throws Exception when unable to create output stream
+     */
+    protected abstract void createOutputStreams() throws Exception;
+
+    /**
+     * Close output streams.
+     *
+     * @throws Exception
+     */
+    protected abstract void closeOutputStreams() throws Exception;
+
+}
---
+++ Core/src/main/java/org/opensextant/output/CSVGenericFormatter.java
@@ -0,0 +1,164 @@
+package org.opensextant.output;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.util.Map;
+
+import org.opensextant.ConfigException;
+import org.opensextant.extraction.ExtractionResult;
+import org.opensextant.processing.Parameters;
+import org.opensextant.processing.ProcessingException;
+import org.supercsv.cellprocessor.Optional;
+import org.supercsv.cellprocessor.ift.CellProcessor;
+import org.supercsv.io.CsvMapWriter;
+import org.supercsv.prefs.CsvPreference;
+
+/**
+ * Alternative to CSVFormatter which is schema-specific and a bit
+ * rigid. ResultsFormatter interface assumes an "extraction result"
+ *
+ * @author ubaldino
+ */
+public class CSVGenericFormatter extends AbstractGenericFormatter {
+
+    private CsvMapWriter writer = null;
+
+    private String delimiter = ",";
+
+    CellProcessor[] outputSchema = null;
+
+    String[] header = null;
+
+    public CSVGenericFormatter(Parameters p) {
+        this.outputExtension = ".csv";
+        this.outputType = "CSV";
+
+        this.setParameters(p);
+        if (p.outputFile != null) {
+            this.setOutputFilename(p.outputFile);
+        }
+
+        defaultFields();
+    }
+
+    public void setDelimiter(String ch) {
+        delimiter = ch;
+    }
+
+    @Override
+    public void start(String nm) throws ProcessingException {
+        try {
+            buildSchema();
+            createOutputStreams();
+            switch (delimiter) {
+            case "\t":
+                writer = new CsvMapWriter(fio, CsvPreference.TAB_PREFERENCE);
+                break;
+            case ",":
+            default:
+                writer = new CsvMapWriter(fio, CsvPreference.STANDARD_PREFERENCE);
+            }
+            writer.writeHeader(header);
+        } catch (Exception err) {
+            throw new ProcessingException("Failed to launch", err);
+        }
+    }
+
+    @Override
+    public void finish() {
+        try {
+            writer.flush();
+            closeOutputStreams();
+        } catch (Exception err) {
+            // error quietly... failed to close.
+            err.printStackTrace();
+        }
+    }
+
+    private FileWriter fio = null;
+
+    @Override
+    protected void createOutputStreams() throws Exception {
+        fio = new FileWriter(new File(getOutputFilepath()));
+    }
+
+    @Override
+    protected void closeOutputStreams() throws Exception {
+        if (writer != null) {
+            writer.close();
+        }
+
+        if (fio != null) {
+            fio.close();
+        }
+    }
+
+    /**
+     * Write the data to the output stream.
+     * @param values Map of data
+     */
+    public void writeRow(Map<String, Object> values) {
+        try {
+            writer.write(values, header, outputSchema);
+        } catch (Exception err) {
+            log.error("Delayed error ERR: {}", err.getLocalizedMessage());
+        }
+    }
+
+    /**
+     * Create a schema instance with the fields properly typed and ordered
+     *
+     * @throws ConfigException
+     *                         schema configuration error
+     */
+    protected void buildSchema() throws ConfigException {
+
+        outputSchema = new CellProcessor[fieldOrder.size()];
+        header = new String[fieldOrder.size()];
+        fieldOrder.toArray(header);
+
+        for (int x = 0; x < fieldOrder.size(); ++x) {
+            outputSchema[x] = new Optional();
+        }
+
+        this.fieldSet.addAll(fieldOrder);
+    }
+
+    /**
+     */
+    protected boolean canAdd(String f) {
+        if (f == null) {
+            return false;
+        }
+        return fieldSet.contains(f);
+    }
+
+    /**
+     * Add a column of data to output; Field is validated ; value is not added
+     * if null
+     */
+    protected void addColumn(Map<String, String> row, String f, String d) {
+        if (d == null) {
+            return;
+        }
+        if (canAdd(f)) {
+            row.put(f, d.toString());
+        }
+    }
+
+    protected void addColumn(Map<String, String> row, String f, int d) {
+        if (canAdd(f)) {
+            row.put(f, Integer.toString(d));
+        }
+    }
+
+    /**
+     * Not implemented: this formatter is more flexible than requiring you to use it
+     * for Extraction output
+     */
+    @Override
+    public String formatResults(ExtractionResult result) throws ProcessingException {
+        throw new ProcessingException("Currently not implemented as an Extraction schema formatter");
+    }
+
+}
