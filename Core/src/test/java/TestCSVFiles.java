53756c899a45574702aaa391a2a8f6042295a868
Marc Ubaldino <ubaldino@mitre.org>
2024-05-07 21:30:19 -0400

    minimize inconsistent license/warning/copyright info.  Just the minimum.   Keep the core stuff in NOTICE file just in one place

---
+++ Core/src/test/java/TestCSVFiles.java
@@ -0,0 +1,2 @@
+package org.opensextant.extractors.test;public class TestCSVFiles {
+}
--- src/main/java/org/opensextant/extractors/geo/PlaceGeocoder.java
+++ src/main/java/org/opensextant/extractors/geo/PlaceGeocoder.java
@@ -1,19 +1,3 @@
-/*
- *
- * Copyright 2013-2021 The MITRE Corporation.
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
- */
 ///~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
 //
 //  _____                                ____                     __                       __
@@ -26,8 +10,8 @@
 //               \ \_\
 //                \/_/
 //
-// OpenSextant PlaceGeocoder (Xponents)
-// Copyright MITRE 2013-2021
+// OpenSextant/Xponents PlaceGeocoder
+// Copyright MITRE Corporation 2013-2024
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
 // */
 package org.opensextant.extractors.geo;
@@ -68,7 +52,7 @@ import org.slf4j.LoggerFactory;
 public class PlaceGeocoder extends GazetteerMatcher
         implements Extractor, CountryObserver, BoundaryObserver, LocationObserver {
 
-    public static final String VERSION = "3.6";
+    public static final String VERSION = "3.7";
     public static final String METHOD_DEFAULT = String.format("PlaceGeocoder v%s", VERSION);
 
     /**
