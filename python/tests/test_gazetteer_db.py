770e584b9614b571d968e76366ea371251d2a1a2
Marc Ubaldino <ubaldino@mitre.org>
2024-05-17 07:53:31 -0400

    moved-to-Xponents-Core

--- python/tests/test_gazetteer_db.py
+++
@@ -1,9 +0,0 @@
-from opensextant.gazetteer import DB, get_default_db
-
-# default DB required, hence this is a unit test.:
-db = DB(get_default_db())
-names = db.list_admin_names()
-print("ADMIN NAMES TOTAL", len(names))
-print("First 10", list(names)[0:10])
-
-db.close()
